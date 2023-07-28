efde8eaa3710dbeb8274e77648ba901bffa7bbe1
radklif <john10733@yandex.ru>
2023-07-28 16:09:03 +0300

    Добавлены view

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('shop/', include('shop.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
--- app/shop/views.py
+++ app/shop/views.py
@@ -1,8 +1,18 @@
+from shop.models import Category
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from shop.serializers import CategorySerializer
+
 
 def index(request):
     return HttpResponse('Дратути!')
 
+
+def category_list(request):
+    if request.method == 'GET':
+        category = Category.objects.all()
+        serializer = CategorySerializer(category, many=True)
+        return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
 
 # Create your views here.
