24d73322e607288e2d59170a3b6ed0a4ce52775e
radklif <john10733@yandex.ru>
2023-07-28 13:20:07 +0300

    Создание модели

--- app/shop/models.py
+++ app/shop/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -22,5 +23,24 @@ from django.utils import timezone
 #
 #     def __str__(self):
 #         return self.choice_text
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Bucket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+
+
+class Order(models.Model):
+    product = models.ManyToManyField(Product)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # Create your models here.
