aee229e3abe21e24356d030dc85d8afa54ddfcee
radklif <john10733@yandex.ru>
2023-07-28 11:40:55 +0300

    Поменял стиль коммента

--- app/shop/models.py
+++ app/shop/models.py
@@ -3,24 +3,24 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-'''
-class Question(models.Model):
-    question_test = models.CharField(max_length=100)
-    pub_date = models.DateTimeField('Date pub')
 
-    def __str__(self):
-        return self.question_test
+# class Question(models.Model):
+#     question_test = models.CharField(max_length=100)
+#     pub_date = models.DateTimeField('Date pub')
+#
+#     def __str__(self):
+#         return self.question_test
+#
+#     def was_published_recently(self):
+#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+#
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=100)
+#     votes = models.IntegerField(default=0)
+#
+#     def __str__(self):
+#         return self.choice_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=100)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
-'''
 # Create your models here.
