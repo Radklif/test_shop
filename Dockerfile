0c845a5a0066c57d58431686154558b26683d888
radklif <john10733@yandex.ru>
2023-10-18 20:14:06 +0300

    Docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-from python:alpine
+FROM python:alpine
 
 ENV WD /app
 ENV AD ./app 
 
 EXPOSE 8000
 WORKDIR ${WD}
-COPY requirements.txt . ${WD} 
+COPY ${AD}/requirements.txt . ${WD} 
 RUN pip install -r requirements.txt
 
 COPY ${AD} ${WD}
-CMD [ "python", "./manage.py", "runserver", "0.0.0.0:8000"]
+#CMD [ "python", "./manage.py", "runserver", "0.0.0.0:8000"]
--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-(&u291j##iw1x+mdi2*v3ctuk+m^t85bpmy7ke@+uyc$0g)6ko
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / STATIC_URL
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
